fdfbe2c7be81190d11e0e547da2dd4b31b44c419
IdrisGuloyev <idrisguloyev@gmail.com>
2022-07-26 21:02:23 +0600

    Форма Ввода Тренировки

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
 const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--temp');
-const inputElevation = document.querySelector('.form__input--climb');
+const inputTemp = document.querySelector('.form__input--temp');
+const inputClimb = document.querySelector('.form__input--climb');
+
+let map, mapEvent;
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
@@ -19,29 +21,18 @@ if (navigator.geolocation) {
 
       const coords = [latitude, longitude];
 
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on('click', function (mapEvent) {
-        const { lat, lng } = mapEvent.latlng;
-
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 200,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('Тренировка')
-          .openPopup();
+      // Обработка кликов на карте
+      map.on('click', function (event) {
+        mapEvent = event;
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
     function () {
@@ -49,3 +40,35 @@ if (navigator.geolocation) {
     }
   );
 }
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  // Очистка полей ввода данных
+  inputDistance.value =
+    inputDuration.value =
+    inputTemp.value =
+    inputClimb.value =
+      '';
+  // Отображение маркера
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 200,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Тренировка')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputClimb.closest('.form__row').classList.toggle('form__row--hidden');
+  inputTemp.closest('.form__row').classList.toggle('form__row--hidden');
+});
